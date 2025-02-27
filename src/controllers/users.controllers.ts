fb17fd9ac8ff8cdc1743360554f76ade3782f02b
thinhtp <thinhtp@greenglobal.vn>
2025-02-27 13:17:00 +0700

    Feat: create user schema

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express'
+import usersService from '~/services/users.services'
 
 export const loginController = (req: Request, res: Response) => {
   const { email, password } = req.body
@@ -6,5 +7,17 @@ export const loginController = (req: Request, res: Response) => {
     res.json({ message: 'Login success' })
   } else {
     res.status(401).json({ error: 'Invalid email or password' })
+  }
+}
+
+export const registerController = async (req: Request, res: Response) => {
+  const { email, password } = req.body
+
+  try {
+    const result = await usersService.register({ email, password })
+
+    res.json({ message: 'Register success', result })
+  } catch (error) {
+    res.status(400).json({ message: 'Register fail', error })
   }
 }
