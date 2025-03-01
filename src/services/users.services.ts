3bb306e398d9e09a791b6f2dc52840a6fcf97e09
thinhtp <thinhtp@greenglobal.vn>
2025-03-01 15:28:00 +0700

    Feat: access token and refresh token

--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -1,13 +1,74 @@
 import User from '~/models/schemas/User.schema'
 import databaseService from './database.services'
+import { RegisterReqBody } from '~/models/requests/User.requests'
+import { hashPassword } from '~/utils/crypto'
+import { signToken } from '~/utils/jwt'
+import { TokenType } from '~/constants/enums'
+
+type Unit =
+  | 'Years'
+  | 'Year'
+  | 'Yrs'
+  | 'Yr'
+  | 'Y'
+  | 'Weeks'
+  | 'Week'
+  | 'W'
+  | 'Days'
+  | 'Day'
+  | 'D'
+  | 'Hours'
+  | 'Hour'
+  | 'Hrs'
+  | 'Hr'
+  | 'H'
+  | 'Minutes'
+  | 'Minute'
+  | 'Mins'
+  | 'Min'
+  | 'M'
+  | 'Seconds'
+  | 'Second'
+  | 'Secs'
+  | 'Sec'
+  | 's'
+  | 'Milliseconds'
+  | 'Millisecond'
+  | 'Msecs'
+  | 'Msec'
+  | 'Ms'
+
+type UnitAnyCase = Unit | Uppercase<Unit> | Lowercase<Unit>
+
+type StringValue = `${number}` | `${number}${UnitAnyCase}` | `${number} ${UnitAnyCase}`
 
 class UsersService {
-  async register(payload: { email: string; password: string }) {
-    const { email, password } = payload
+  private signAccessToken(user_id: string) {
+    return signToken({
+      payload: { user_id, token_type: TokenType.AccessToken },
+      options: { expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN as StringValue }
+    })
+  }
 
-    const result = await databaseService.users.insertOne(new User({ email, password }))
+  private signRefreshToken(user_id: string) {
+    return signToken({
+      payload: { user_id, token_type: TokenType.RefreshToken },
+      options: { expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN as StringValue }
+    })
+  }
 
-    return result
+  async register(payload: RegisterReqBody) {
+    const result = await databaseService.users.insertOne(
+      new User({ ...payload, date_of_birth: new Date(payload.date_of_birth), password: hashPassword(payload.password) })
+    )
+
+    const user_id = result.insertedId.toString()
+    const [access_token, refresh_token] = await Promise.all([
+      this.signAccessToken(user_id),
+      this.signRefreshToken(user_id)
+    ])
+
+    return { access_token, refresh_token }
   }
 
   async checkEmailExist(email: string) {
