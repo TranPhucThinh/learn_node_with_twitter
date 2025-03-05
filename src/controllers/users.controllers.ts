38422c35718426983750ead40995de5e987be389
thinhtp <thinhtp@greenglobal.vn>
2025-03-05 22:07:46 +0700

    Feat: update logic login

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,16 +1,17 @@
 import { NextFunction, Request, Response } from 'express'
 import { ParamsDictionary } from 'express-serve-static-core'
-import { ErrorWithStatus } from '~/models/Errors'
+import { ObjectId } from 'mongodb'
+import { USERS_MESSAGES } from '~/constants/messages'
 import { RegisterReqBody } from '~/models/requests/User.requests'
+import User from '~/models/schemas/User.schema'
 import usersService from '~/services/users.services'
 
-export const loginController = (req: Request, res: Response) => {
-  const { email, password } = req.body
-  if (email === 'thinh@gmail.com' && password === '123123') {
-    res.json({ message: 'Login success' })
-  } else {
-    res.status(401).json({ error: 'Invalid email or password' })
-  }
+export const loginController = async (req: Request, res: Response) => {
+  const user = req.user as User
+  const user_id = user._id as ObjectId
+  const result = await usersService.login(user_id.toString())
+
+  res.json({ message: USERS_MESSAGES.LOGIN_SUCCESS, result })
 }
 
 export const registerController = async (
@@ -20,5 +21,5 @@ export const registerController = async (
 ) => {
   const result = await usersService.register(req.body)
 
-  res.json({ message: 'Register success', result })
+  res.json({ message: USERS_MESSAGES.REGISTER_SUCCESS, result })
 }
--- src/middlewares/users.middlewares.ts
+++ src/middlewares/users.middlewares.ts
@@ -1,36 +1,67 @@
-import { NextFunction, Request, Response } from 'express'
 import { checkSchema } from 'express-validator'
-import { ErrorWithStatus } from '~/models/Errors'
+import { USERS_MESSAGES } from '~/constants/messages'
+import databaseService from '~/services/database.services'
 import usersService from '~/services/users.services'
+import { hashPassword } from '~/utils/crypto'
 import { validate } from '~/utils/validation'
 
-export const loginValidator = (req: Request, res: Response, next: NextFunction) => {
-  const { email, password } = req.body
-  if (!email || !password) {
-    res.status(400).json({ error: 'Missing email or password' })
-  } else {
-    next()
-  }
-}
+export const loginValidator = validate(
+  checkSchema({
+    email: {
+      isEmail: { errorMessage: USERS_MESSAGES.EMAIL_IS_INVALID },
+      trim: true,
+      custom: {
+        options: async (value, { req }) => {
+          const user = await databaseService.users.findOne({ email: value, password: hashPassword(req.body.password) })
+
+          if (user === null) {
+            throw new Error(USERS_MESSAGES.EMAIL_OR_PASSWORD_INCORRECT)
+          }
+          req.user = user
+
+          return true
+        }
+      }
+    },
+    password: {
+      notEmpty: { errorMessage: USERS_MESSAGES.PASSWORD_IS_REQUIRED },
+      isString: { errorMessage: USERS_MESSAGES.PASSWORD_MUST_BE_STRING },
+      isLength: { options: { min: 6, max: 50 }, errorMessage: USERS_MESSAGES.PASSWORD_LENGTH_MUST_BE_FROM_6_TO_50 },
+      isStrongPassword: {
+        options: {
+          minLength: 6,
+          minLowercase: 1,
+          minUppercase: 1,
+          minNumbers: 1,
+          minSymbols: 1
+        },
+        errorMessage: USERS_MESSAGES.PASSWORD_MUST_BE_STRONG
+      }
+    }
+  })
+)
 
 export const registerValidator = validate(
   checkSchema({
     name: {
-      notEmpty: true,
-      isString: true,
-      isLength: { options: { min: 3, max: 100 } },
+      notEmpty: {
+        errorMessage: USERS_MESSAGES.NAME_IS_REQUIRED
+      },
+      isString: {
+        errorMessage: USERS_MESSAGES.NAME_MUST_BE_STRING
+      },
+      isLength: { options: { min: 3, max: 100 }, errorMessage: USERS_MESSAGES.NAME_LENGTH_MUST_BE_FROM_1_TO_100 },
       trim: true
     },
     email: {
-      notEmpty: true,
-      isEmail: true,
+      isEmail: { errorMessage: USERS_MESSAGES.EMAIL_IS_INVALID },
       trim: true,
       custom: {
         options: async (value) => {
           const isExistEmail = await usersService.checkEmailExist(value)
 
           if (isExistEmail) {
-            throw new Error('Email already exists')
+            throw new Error(USERS_MESSAGES.EMAIL_ALREADY_EXISTS)
           }
 
           return true
@@ -38,9 +69,9 @@ export const registerValidator = validate(
       }
     },
     password: {
-      notEmpty: true,
-      isString: true,
-      isLength: { options: { min: 6, max: 50 } },
+      notEmpty: { errorMessage: USERS_MESSAGES.PASSWORD_IS_REQUIRED },
+      isString: { errorMessage: USERS_MESSAGES.PASSWORD_MUST_BE_STRING },
+      isLength: { options: { min: 6, max: 50 }, errorMessage: USERS_MESSAGES.PASSWORD_LENGTH_MUST_BE_FROM_6_TO_50 },
       isStrongPassword: {
         options: {
           minLength: 6,
@@ -49,14 +80,16 @@ export const registerValidator = validate(
           minNumbers: 1,
           minSymbols: 1
         },
-        errorMessage:
-          'Password must be at least 6 characters long and contain at least 1 lowercase letter, 1 uppercase letter, 1 number, and 1 symbol'
+        errorMessage: USERS_MESSAGES.PASSWORD_MUST_BE_STRONG
       }
     },
     confirm_password: {
-      notEmpty: true,
-      isString: true,
-      isLength: { options: { min: 6, max: 50 } },
+      notEmpty: { errorMessage: USERS_MESSAGES.CONFIRM_PASSWORD_IS_REQUIRED },
+      isString: { errorMessage: USERS_MESSAGES.CONFIRM_PASSWORD_MUST_BE_STRING },
+      isLength: {
+        options: { min: 6, max: 50 },
+        errorMessage: USERS_MESSAGES.CONFIRM_PASSWORD_LENGTH_MUST_BE_FROM_6_TO_50
+      },
       isStrongPassword: {
         options: {
           minLength: 6,
@@ -65,13 +98,12 @@ export const registerValidator = validate(
           minNumbers: 1,
           minSymbols: 1
         },
-        errorMessage:
-          'Password must be at least 6 characters long and contain at least 1 lowercase letter, 1 uppercase letter, 1 number, and 1 symbol'
+        errorMessage: USERS_MESSAGES.CONFIRM_PASSWORD_MUST_BE_STRONG
       },
       custom: {
         options: (value, { req }) => {
           if (value !== req.body.password) {
-            throw new Error('Passwords confirmation does not match password!')
+            throw new Error(USERS_MESSAGES.CONFIRM_PASSWORD_IS_NOT_MATCH)
           } else {
             return true
           }
@@ -83,7 +115,8 @@ export const registerValidator = validate(
         options: {
           strict: true,
           strictSeparator: true
-        }
+        },
+        errorMessage: USERS_MESSAGES.DATE_OF_BIRTH_MUST_IS_ISO8601
       }
     }
   })
